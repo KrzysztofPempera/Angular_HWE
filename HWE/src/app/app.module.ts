0d6bc287a6f9ca3552561bfd2fe060b1827bec35
Krzysztof Pempera <krzpem@st.amu.edu.pl>
2020-03-27 22:32:03 +0100

    Initial commit

---
+++ HWE/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { CounterComponent } from './counter/counter.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavMenuComponent,
+    FetchDataComponent,
+    CounterComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
